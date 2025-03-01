b41b3ef6558ae3214b039405791f48952b34b31a
Gilang2220 <ramdhanigilang672@gmail.com>
2025-03-01 21:57:44 +0700

    menambahkan footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
+import Footer from './Components/Footer/footer'
 import Navbar from './Components/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Footer/>
     </>
   )
 }
