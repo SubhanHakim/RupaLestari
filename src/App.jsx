44281235b97db8919b9a0ebaaf73064df79e7684
Subhan Hakim <subhanhakim07@gmail.com>
2025-03-05 13:42:37 +0700

    fix merge development

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,9 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Product from "./pages/product";
 
-
-function App() {
-  return (
-
-    <Router>
-      <nav>
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Product from "./pages/product";
-
 const App = () => {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,3 @@
-
 import Bestseller from "../Components/Bestseller/Bestseller";
 import Categories from "../Components/Categories/Categories";
 import Hero from "../Components/Heros/Hero";
@@ -6,24 +5,19 @@ import Navbar from "../Components/Navbar/Navbar";
 import HeroMid from "../Components/Hero2";
 import Trends from "../Components/Trends";
 
-
 const Home = () => {
   return (
     <div>
-
       <Navbar />
-        <div className="bg-[#2C3E50]">
-            <Hero />
-        </div>
-        <div className="px-3">
+      <div className="bg-[#2C3E50]">
+        <Hero />
+      </div>
+      <div className="px-3">
         <Categories />
         <Bestseller />
-        </div>
-    </div>
-  )
-      <h1>Home Page</h1>
-      <HeroMid />
-      <Trends />
+        <HeroMid />
+        <Trends />
+      </div>
     </div>
   );
 };
